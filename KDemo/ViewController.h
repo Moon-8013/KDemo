1d489a30e59403dd8ce5085db4a51fd011a96dba
swifttest <W@w.local>
2018-03-21 09:34:41 +0800

    Initial Commit

---
+++ KDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KDemo
+//
+//  Created by W on 2018/3/21.
+//  Copyright © 2018年 wangting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KDemo
+//
+//  Created by W on 2018/3/21.
+//  Copyright © 2018年 wangting. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
